a78f0dd7c9f1e28dcc586c8d48f998eb80ce150f
Muhammad Yasir <44177978+yyasirnawaz1@users.noreply.github.com>
2020-04-20 13:19:58 +0100

    bundling and project cleanup

---
+++ LTC_Dashboard/wwwroot/ScriptsView/Home.js
@@ -0,0 +1,39 @@
+﻿
+
+var HomeView = function () {
+
+    return {
+        
+        init: function () {
+ 
+        },
+
+        getScheduledNewsLetterStatistics: function ()
+        {
+            Layout.showLoader(); 
+            var category = $('#Status').val();
+            var period = $('#Period').val();
+            var actionURL = '/Newsletter/GetScheduledNewsLetterStatistics' + '?category=' + category + '&period=' + period;
+            $.get(actionURL, function (data) {
+                
+                $('#scheduledNewsletterContainer').html(data);
+
+            });
+
+        },
+         
+    };
+
+}();
+
+HomeView.init();
+
+$(document).ready(function () {
+    HomeView.getScheduledNewsLetterStatistics();
+    $('#Status').on("change", function () { HomeView.getScheduledNewsLetterStatistics(); });
+    $('#Period').on("change", function () { HomeView.getScheduledNewsLetterStatistics(); });
+    $('#reloadnewsletterstat').on("click", function () { HomeView.getScheduledNewsLetterStatistics(); });
+
+});
+
+
---
+++ LTC_Dashboard/wwwroot/ScriptsView/Preferences.js
@@ -0,0 +1,101 @@
+﻿$.ajaxSetup({ cache: false });
+var Preferences = function () {
+
+
+    return {
+
+
+        clearChangePasswordForm: function () {
+
+            $("#formchangepassword div").removeClass("error");
+            $("#formchangepassword label").removeClass("error");
+            $("#formchangepassword")[0].reset();
+
+        }
+
+
+
+
+
+    };
+
+}();
+
+function passwordChangeSuccess(data) {
+    var statusCode = data.StatusCode;
+    switch (statusCode) {
+        case 1:
+            {
+
+                ltcApp.successMessage(null, data.StatusMessage);
+                Preferences.clearChangePasswordForm();
+                break;
+            }
+        case 0:
+            {
+                ltcApp.errorMessage(data.StatusMessage);
+                break;
+            }
+        default:
+            {
+
+                ltcApp.errorMessage(data.StatusMessage);
+                break;
+            }
+    }
+}
+
+function userdetailSuccess(data) {
+    var statusCode = data.StatusCode;
+    switch (statusCode) {
+        case 1:
+            {
+
+                ltcApp.successMessage(null, data.StatusMessage);
+                Preferences.clearChangePasswordForm();
+                break;
+            }
+        case 0:
+            {
+                ltcApp.errorMessage(data.StatusMessage);
+                break;
+            }
+        default:
+            {
+
+                ltcApp.errorMessage(data.StatusMessage);
+                break;
+            }
+
+    }
+
+}
+
+
+function officedetailSuccess(data) {
+    var statusCode = data.StatusCode;
+    switch (statusCode) {
+        case 1:
+            {
+
+                ltcApp.successMessage(null, data.StatusMessage);
+                Preferences.clearChangePasswordForm();
+                break;
+            }
+        case 0:
+            {
+                ltcApp.errorMessage(data.StatusMessage);
+                break;
+            }
+        default:
+            {
+
+                ltcApp.errorMessage(data.StatusMessage);
+                break;
+            }
+
+    }
+
+}
+
+
